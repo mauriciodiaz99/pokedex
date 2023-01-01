4072908ff1dd99aeb32342c2e4ce3773af2f2769
mauriciodiaz99 <dwdmmadm2001@gmail.com>
2022-12-31 21:26:56 -0600

    Tarea1 Terminada

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,9 +1,16 @@
+import { IsInt, isInt, IsPositive, IsString, Min, MinLength } from "class-validator";
+
 export class CreatePokemonDto {
 
     // isInt, IsPositive, min 1
+    @IsInt()
+    @Min(1)
+    @IsPositive()
     no: number;
 
     // isString, Minlenth 1
+    @IsString()
+    @MinLength(1)
     name: string;
 
 }
